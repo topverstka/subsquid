937afb0cd969d4c8830ca8eaaa40a868c7fb0d2f
viridius-hub <smiledie-hub@yandex.ru>
2023-08-03 20:53:24 +0500

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,32 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import "./App.scss"
+import Builders from "./routes/builders";
+import Appchain from "./routes/appchain";
+import Aquarium from "./routes/aquarium";
+import Network from "./routes/network";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root/>,
         errorElement: <ErrorPage/>,
+    },
+    {
+        path: "/builders",
+        element: <Builders/>,
+    },
+    {
+        path: "/appchain",
+        element: <Appchain/>,
+    },
+    {
+        path: "/aquarium",
+        element: <Aquarium/>,
+    },
+    {
+        path: "/network",
+        element: <Network/>,
     },
 ]);
 
